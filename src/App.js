cbfe5aa9965224f35c6db39fea568b0b23b1a089
Kanishk <Kanishksharma2609@gmail.com>
2023-08-05 22:14:20 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import LandingPage from './Pages/LandingPage';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <LandingPage></LandingPage>
+  
+    </>
   );
 }
 
