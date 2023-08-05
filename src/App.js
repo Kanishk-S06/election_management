c7aa23043c746115a713835892860c59faa936ef
Kanishk <Kanishksharma2609@gmail.com>
2023-08-06 01:11:17 +0530

    sifnreinv

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import "./App.css";
-import LandingPage from "./Pages/LandingPage";
+// import LandingPage from "./Pages/LandingPage";
+import InfoSection from "./Google-Maps/InfoSection";
+// import Map from "./Google-Maps/Map_Api";
 
 
 function App() {
   return (
     <>
-      <LandingPage></LandingPage>
+      {/* <LandingPage></LandingPage> */}
+      <InfoSection></InfoSection>
+      {/* <Map></Map> */}
+
 
     </>
   );
