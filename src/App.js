a63dca167fc04b70f6e255883be1f78759918944
mohdshahzil <theshahzil@gmail.com>
2023-08-06 01:13:35 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-
 import "./App.css";
+import React from "react";
+import ReactDOM from "react-dom";
 import LandingPage from "./Pages/LandingPage";
-
+import Map from "./Google-Maps/Map_Api";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <LandingPage></LandingPage>
-
-    </>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Map />} />
+        <Route path="*" element={<LandingPage />} />
+      </Routes>
+    </Router>
   );
 }
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 export default App;
